f93d81e1a140330d7f383dbd09a188f51480c1d6
liubavabarsegian <lyubahemmo@mail.ru>
2023-04-10 10:21:29 +0300

    user interface

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,26 @@
+class TripsController < ApplicationController
+  def show
+    sql = "Select * from trips"
+    @records_array =  ActiveRecord::Base.connection.exec_query(sql)
+  end
+
+  def query
+      sql = params[:select]
+      @records_array =  ActiveRecord::Base.connection.exec_query(sql)
+  end
+
+
+  def add_data
+    # (0...100).each do 
+    #   departure_flight = Flight.where.not(departure_time: Flight.maximum('departure_time')).sample
+    #   departure_date = departure_flight.departure_time
+    #   arrival_flight = Flight.where(departure_time: departure_date...Flight.maximum('departure_time')).sample
+    #   arrival_date = arrival_flight.departure_time
+    #   hotel = Hotel.ids.sample
+    #   meal = ["Room Only", "Bed and Breakfast", "Half Board", "Half Board plus", "Full Board", "Full Board plus", "All inclusive", "Ultra All Inclusive"].sample
+    #   Trip.create!({departure_date: departure_date, arrival_date: arrival_date, departure_flight_id: departure_flight.id, arrival_flight_id: arrival_flight.id, meal: meal, hotel_id: hotel})
+    # end
+
+    redirect_to trips_show_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  root 'home#schema'
+  get 'home/info'
+
+  get 'clients/show'
+  get 'workers/show'
+  get 'hotels/show'
+  get 'trips/show'
+  get 'countries/show'
+  get 'cities/show'
+  get 'airports/show'
+  get 'aircompanies/show'
+  get 'flights/show'
+  get 'orders/show'
+
+  post 'clients/query'
+  post 'workers/query'
+  post 'hotels/query'
+  post 'countries/query'
+  post 'cities/query'
+  post 'airports/query'
+  post 'aircompanies/query'
+  post 'flights/query'
+  post 'trips/query'
+  post 'orders/query'
+
+  post 'clients/add_data'
+  post 'workers/add_data'
+  post 'countries/add_data'
+  post 'cities/add_data'
+  post 'hotels/add_data'
+  post 'airports/add_data'
+  post 'aircompanies/add_data'
+  post 'flights/add_data'
+  post 'trips/add_data'
+  post 'orders/add_data'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
